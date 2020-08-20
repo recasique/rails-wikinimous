48730a7d047dc7184d8ce67b1bcb1534169f5fbf
Rafael Casique <recasique@gmail.com>
2020-08-20 18:03:57 +0200

    back end done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+# puts 'Creating new articles' just feedback
+
+10.times do
+  Article.create(title: Faker::Movies::StarWars.character, content: Faker::Movies::StarWars.quote)
+end
